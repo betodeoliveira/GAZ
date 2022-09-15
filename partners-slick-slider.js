4b8dbc880e58589e02cdaf3295b476a7468286be
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-15 14:25:20 -0300

    Initial config

---
+++ partners-slick-slider.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    $('.partners_list').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+});
--- url-param-select-tab.js
+++ url-param-select-tab.js
@@ -1,1 +1,14 @@
+var Webflow = Webflow || [];
+Webflow.push(function () {
+  var tabName = getParam('tab');
+  if (!tabName) return;
 
+  $('.' + tabName).triggerHandler('click');
+
+  function getParam(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+      results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
+});
